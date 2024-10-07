cb1d024f19f4d7892f86d800d33ff3eb4a138244
noammirjani <noamirjani@gmail.com>
2024-10-07 16:54:15 +0300

    add task manager class

--- organize_me/task.py
+++ organize_me/task.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Optional, ClassVar
+from typing import Optional, ClassVar, Any
 from pydantic import BaseModel, Field, field_validator, model_validator
 
 
@@ -54,7 +54,7 @@ class Task(BaseModel):
     # noinspection PyNestedDecorators
     @model_validator(mode='before')
     @classmethod
-    def validate_date_range(cls, values: dict) -> dict:
+    def validate_date_range(cls, values: dict[str, Optional[datetime]]) -> dict[str, Optional[datetime]]:
         start_date = values.get('start_date')
         end_date = values.get('end_date')
         if (start_date and not end_date) or (end_date and not start_date):
@@ -63,17 +63,17 @@ class Task(BaseModel):
             raise ValueError(cls.ERROR_END_DATE_BEFORE_START)
         return values
 
-    def __setattr__(self, key, value):
+    def __setattr__(self, key: str, value: Any) -> None:
         super().__setattr__(key, value)
         if key != 'update_date':
             self.update_date = datetime.now()
 
-    def update(self, **kwargs) -> None:
+    def update(self, **kwargs: Any) -> None:
         for field, value in kwargs.items():
             if value is not None and field in self.model_fields:
                 setattr(self, field, value)
         self.__validate__()
 
     def __validate__(self) -> None:
-        # self.__class__.validate(self.__dict__)
-        Task.model_validate(self.model_dump())
+        self.__class__.model_validate(self.model_dump())
+        # Task.model_validate(self.model_dump())
